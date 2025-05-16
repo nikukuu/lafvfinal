6364f4e27f41c4a9cf241c9567c37019f3c63966
nikukuu <nicolemahilum2@gmail.com>
2025-05-17 07:47:54 +0800

    Update items_bp.py

--- blueprints/items_bp.py
+++ blueprints/items_bp.py
@@ -5,10 +5,10 @@ items_bp = Blueprint('items', __name__, template_folder='../templates')
 
 def get_db_connection():
     connection = mysql.connector.connect(
-            host='localhost',
-            user='root',
-            password='',
-            database='lafsample'
+            host='kwimn.h.filess.io',
+            user='lostthenfound_gashorndog',
+            password='1c2943e986bcf6276ea1c71099e14054ae2b9283',
+            database='lostthenfound_gashorndog'
         )
     return connection
 
