a694bc51b5f4185961639a8108f832a549b8afe0
nikukuu <nicolemahilum2@gmail.com>
2025-05-16 19:27:30 +0800

    Update admin_bp.py

--- blueprints/admin_bp.py
+++ blueprints/admin_bp.py
@@ -11,10 +11,11 @@ admin_bp = Blueprint('admin', __name__, template_folder='../templates')
 def get_db_connection():
     try:
         connection = mysql.connector.connect(
-            host='sql12.freesqldatabase.com',
-            user='sql12752537',
-            password='HmcHn7eXlU',
-            database='sql12752537'
+            host='kwimn.h.filess.io',
+            user='lostthenfound_gashorndog',
+            password='1c2943e986bcf6276ea1c71099e14054ae2b9283',
+            database='lostthenfound_gashorndog'
+            port = "3307"
         )
         return connection
     except mysql.connector.Error as e:
